ed9b26acd2f84aa78ff1e43a6829b4cff8fbd3a5
rifkiaz <rifki.affandi3@gmail.com>
2020-10-12 17:19:53 +0700

    initial commit

--- connect.py
+++ connect.py
@@ -3,7 +3,7 @@ import mysql.connector
 db = mysql.connector.connect(
     host="localhost",
     user="admin",
-    passwd="1521"
+    passwd="tehanget"
 )
 if db.is_connected():
     print("Yey berhasil terhubung")
